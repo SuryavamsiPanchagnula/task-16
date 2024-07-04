7b232cccf731443bf1adf18afedb5edc1fadd715
unknown <psuryavamsi88@gmail.com>
2024-07-05 00:56:03 +0530

    task-16 selenium locators

---
+++ selenium_locators/src/test/java/selenium_locators/q1.java
@@ -0,0 +1,24 @@
+package selenium_locators;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class q1 {
+
+	public static void main(String args[]) {
+		
+		WebDriverManager.firefoxdriver().setup();
+		
+		WebDriver driver = new FirefoxDriver();
+	
+		driver.get("https://www.google.com/");
+		
+		driver.navigate().refresh();
+		
+		System.out.println(driver.getCurrentUrl());
+		
+		driver.close();
+	}
+}
